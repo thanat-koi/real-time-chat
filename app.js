fb6ff78de2d066dc9eefd95dbab60d815c8c9d91
thanat-koi <57722046+thanat-koi@users.noreply.github.com>
2020-07-17 22:24:43 +0700

    almost

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const socketio = require('socket.io');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render("index");
+});
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log('server is run');
+
+});
+
+const io = socketio(server);
+
+io.on('connection', socket => {
+    console.log('New connected');
+    
+    socket.username = 'Anonymous'
+
+    socket.on('change_username', data => {
+        socket.username = data.username
+    })
+    socket.on('new_message', data => {
+        console.log('new message');
+        io.sockets.emit('receive_message', {message: data.message, username: socket.username});
+    })
+});
